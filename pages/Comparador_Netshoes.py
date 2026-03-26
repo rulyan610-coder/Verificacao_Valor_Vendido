0dc893704b87ec9df6103afce2fb586d76a470aa
RulyanDEV <rulyan.610@gmail.com>
2026-03-26 08:56:07 -0300

    Adicionado Comparador Generico, criação da página inicial(correcao03)

--- pages/Comparador_Netshoes.py
+++ pages/Comparador_Netshoes.py
@@ -14,6 +14,7 @@ st.set_page_config(
     page_title="Comparador de SKUs e Preços",
     page_icon="🔍",
     layout="wide",
+    initial_sidebar_state="collapsed"
 )
 
 # ── CSS customizado ───────────────────────────────────────────────────────────
